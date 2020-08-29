ce86185dcbd4c087d49498a5f6de54b947574830
Vicente Lizana <v.lizana.e@gmail.com>
2020-08-29 17:47:28 -0400

    Set up and minimal example.

--- setup.py
+++ setup.py
@@ -10,23 +10,28 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+with open('requirements.txt') as requirements_file:
+    requirements = [
+        line.strip() for line in requirements_file.readlines() if line.strip()
+    ]
 
-setup_requirements = [ ]
+with open('requirements_dev.txt') as requirements_file:
+    setup_requirements = [
+        line.strip() for line in requirements_file.readlines() if line.strip()
+    ]
 
 test_requirements = [ ]
 
 setup(
     author="Vicente Lizana Estivill",
     author_email='v.lizana.e@gmail.com',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
--- tests/test_pyntual.py
+++ tests/test_pyntual.py
@@ -4,18 +4,24 @@
 
 
 import unittest
+import pandas as pd
 
-from pyntual import pyntual
+from pyntual import api
 
 
-class TestPyntual(unittest.TestCase):
+class TestPyntualAPI(unittest.TestCase):
     """Tests for `pyntual` package."""
 
     def setUp(self):
         """Set up test fixtures, if any."""
+        pass
 
     def tearDown(self):
         """Tear down test fixtures, if any."""
+        pass
 
-    def test_000_something(self):
-        """Test something."""
+    def test_001_asset_providers(self):
+        """Test asset providers API call."""
+        providers = api.asset_providers(1)
+        self.assertIsInstance(providers, pd.DataFrame, 'Response must be DataFrame')
+        self.assertListEqual(providers.columns, ['name'], 'DataFrame must have name column')
